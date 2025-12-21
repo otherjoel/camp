600e2ec825ebe4821bb62fa90bcda4510022c700
Joel Dueck <joel@jdueck.net>
2025-12-21 09:26:29 -0600

    Fix demo "series" page

--- camp-demo/pages/series.md.rkt
+++ camp-demo/pages/series.md.rkt
@@ -1,18 +1,18 @@
-#lang punct camp-demo
+#lang camp/page
 
----
-title: Browse by Series
-slug: series
----
+#:title "Browse by Series"
+#:slug "series"
 
-Some posts are part of ongoing series:
+(define series-pages (get-taxonomy-pages "blog" "series"))
 
-•(let ([series-pages (get-taxonomy-pages "blog" "series")])
-   `(div ((block "root") (class "taxonomy-listing"))
-      ,@(for/list ([s (get-taxonomy-terms "blog" "series")])
-          `(section ()
-             (h2 () ,s)
-             (ul ()
-               ,@(for/list ([pg (hash-ref series-pages s)])
-                   `(li () (a ((href ,(page-link-url pg)))
-                              ,(page-link-title pg)))))))))
+`(div ((class "content"))
+   (h1 "Browse by Series")
+   (p "Some posts are part of ongoing series:")
+   (div ((class "taxonomy-listing"))
+     ,@(for/list ([s (get-taxonomy-terms "blog" "series")])
+         `(section ((class "series-section"))
+            (h2 ,s)
+            (ul
+              ,@(for/list ([pg (hash-ref series-pages s)])
+                  `(li (a ((href ,(page-link-url pg)))
+                          ,(page-link-title pg)))))))))
